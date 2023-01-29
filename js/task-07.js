21103be41d2dfccfd8d57ee2df8c3e903678256f
Anna Trifonova <3809322999@ukr.net>
2023-01-29 12:02:51 +0200

    change task-7 and task-9

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const range = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 
-range.addEventListener('change', onRangeChange);
+range.addEventListener('input', onRangeChange);
 
 function onRangeChange(event) {
 	text.style.fontSize = `${event.currentTarget.value}px`;
